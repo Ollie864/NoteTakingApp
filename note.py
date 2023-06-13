83781116c656ff9d5a8dfd9c1ca03bf45a4609d7
Oliver Lambert <olambert684@protonmail.com>
2023-06-13 16:50:09 +0100

    added ability to save notes to database

--- note.py
+++ note.py
@@ -12,6 +12,79 @@ class MainWindow(QMainWindow):
         uic.loadUi("ui/main.ui", self)
         self.show()
 
+        self.currentlyLoadedTitle = ""
+        self.isNewNote = True
+
+        self.actionQuit.triggered.connect(exit)
+        self.actionSave.triggered.connect(self.saveNote)
+
+    def saveNote(self):
+        conn = sqlite3.connect("notelist.db")
+        c = conn.cursor()
+
+        itemTitle = self.textBoxTitle.text()
+        itemContent = self.textBoxContent.toPlainText()
+        items = [itemTitle, itemContent]
+
+        if itemTitle == "":
+            errorMessage1 = QMessageBox()
+            errorMessage1.setText("Note needs a title")
+            errorMessage1.exec_()
+            return 0
+
+        if self.isNewNote:
+            # Check if note with the same title exists in database
+            check = c.execute(
+                'SELECT title FROM noteList WHERE title = ?', (itemTitle,))
+            checkValue = c.fetchone()
+            if checkValue:
+                errorMessage = QMessageBox()
+                errorMessage.setText("Note with that name already exists")
+                errorMessage.exec_()
+            else:
+                c.execute("""INSERT INTO noteList (title, content)
+                      VALUES (?, ?)
+                      """, items)
+        else:
+            # Delete old note and save new one in its place
+            c.execute("DELETE FROM noteList WHERE title = ?",
+                      (self.currentlyLoadedTitle,))
+            c.execute("""INSERT INTO noteList (title, content)
+                      VALUES (?, ?)
+                      """, items)
+
+        self.currentlyLoadedTitle = itemTitle
+        conn.commit()
+        conn.close()
+        self.loadNotesTitle()
+
+    def deleteNote(self):
+        pass
+
+    def newNote(self):
+        pass
+
+    def backupNote(self):
+        pass
+
+    def loadNotes(self):
+        pass
+
+    def loadNotesTitle(self):
+        self.notesTitleListWidget.clear()
+        conn = sqlite3.connect("notelist.db")
+        c = conn.cursor()
+
+        c.execute("SELECT title FROM noteList")
+
+        databaseTitles = c.fetchall()
+        conn.commit()
+        conn.close()
+
+        # loop through and add to
+        for i in databaseTitles:
+            self.notesTitleListWidget.addItem(str(i[0]))
+
 
 # Initialise the database
 conn = sqlite3.connect("notelist.db")
